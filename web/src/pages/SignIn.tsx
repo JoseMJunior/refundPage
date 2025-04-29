ff1228e9cd63dafdb6940dd591403708163aab96
José Martins <154391835+JoseMJunior@users.noreply.github.com>
2025-04-28 21:38:27 -0300

    start-sign in

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,5 +1,21 @@
+import { Input } from "../components/Input";
+
 export function SigIn(){
     return(
-        <h1> cadastro</h1>
+        <form className="w-full flex flex-col gap-4"> 
+            <Input
+                required
+                legend="E-mail"
+                type="email"
+                placeholder="seu@email"
+            />
+            <Input
+                required
+                legend="Senha"
+                type="password"
+                placeholder="senha"
+            />
+
+        </form>
     )
 }
