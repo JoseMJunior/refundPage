4a81d30c7986923c15e2ac09d735b79ddbe65367
José Martins <154391835+JoseMJunior@users.noreply.github.com>
2025-05-03 12:19:45 -0300

    Refund/Confirm pages

--- web/src/pages/Refund.tsx
+++ web/src/pages/Refund.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 import { CATEGORIES, CATEGORIES_KEYS} from "../utils/categories"
 
 export function Refund(){
-    const [category,SetCategory] = useState("")
+    const [name,setName] = useState("")
+    const [category,setCategory] = useState("")
+    const [amount,setAmount] = useState("")
+    const [isLoading,setIsLoading] = useState(false)
+    const [filename,setFilename] = useState<File | null>(null)
+    const navigate = useNavigate()
+
+    function onSubmit(e: React.FormEvent){
+        e.preventDefault()
+        console.log(name, category, amount, filename)
+        navigate("/confirm", { state: {fromSubmit:true}})
+    }
 
     return (
-        <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+        <form 
+            onSubmit={onSubmit}
+            className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+        >
             <header>
                 <h1 className="text-xl font-bold text-gray-100">
                     Solicitação de reembolso
@@ -20,23 +37,47 @@ export function Refund(){
             <Input
                 required
                 legend="Nome da solicitação"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
             />
 
-            <Select
-                required
-                legend="Categoria"
-                value={category}
-                onChange={(e) => SetCategory(e.target.value)}
-            >
-                {CATEGORIES_KEYS.map((category) => (
-                    <option
+            <div className="flex gap-4">
+                <Select
+                    required
+                    legend="Categoria"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                    {CATEGORIES_KEYS.map((category) => (
+                        <option
                         key={category}
                         value={category}
-                    >
-                        {CATEGORIES[category].name}
-                    </option>
-                ))}
-            </Select>
+                        >
+                            {CATEGORIES[category].name}
+                        </option>
+                    ))}
+                </Select>
+
+                <Input
+                    required
+                    legend="Valor"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+            </div>
+
+            <Upload 
+                filename={filename && filename.name} 
+                onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+            />
+
+            <Button 
+                type="submit"
+                isLoading={isLoading}
+            >
+                Enviar
+            </Button>
+            
         </form>
     )
 }
--- web/src/routes/EmployeeRoute.tsx
+++ web/src/routes/EmployeeRoute.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { NOTFound } from "../pages/NOTFound";
 import { Refund } from "../pages/Refund";
 import { AppLayout } from "../components/AppLayout";
+import { Confirm } from "../pages/Confirm";
 
 
 
@@ -12,6 +13,7 @@ export function EmpoyeeRoutes(){
         <Routes>
             <Route path="/" element={<AppLayout/>}>
                 <Route path="/" element={<Refund/>}/>
+                <Route path="/confirm" element={<Confirm/>}/>
             </Route>
 
             <Route path="*" element={<NOTFound/>}/>
